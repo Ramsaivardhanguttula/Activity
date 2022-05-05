a32ac99987b57748c6d696e27353a8777801dc41
Guttula Ram Sai Vardhan <98812447+Ramsaivardhanguttula@users.noreply.github.com>
2022-05-05 12:35:04 +0530

    Update stringtoHexa.c

--- stringtoHexa.c
+++ stringtoHexa.c
@@ -5,12 +5,12 @@
 int stringtoHexa(char name[])
 {
     int i,l,sum= 0;
-    char *x=name;
-    l= strlen(name);
-    int arr1[l];
+    char *x=name;// pointer which points to the base address of the string initially
+    l= strlen(name);//length of the string is stored into l variable
+    int arr1[l];//creating a new array
     for(i=0;i<=l;i++)
     {
-        arr1[i]=(int)*(x+i);
+        arr1[i]=(int)*(x+i);// Here pointer gets incremented and points to the next address and typecasting into and and storing into array
         sum=sum+arr1[i];
     }
     return sum;
